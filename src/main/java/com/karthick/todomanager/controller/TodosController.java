b23a6508bc154d53d7df9e7497cd22ff15555612
“Karthick-Ramesh-16” <karthick.ramesh@freshworks.com>
2021-12-26 13:01:26 +0530

    Set up MySql to Todo Manager

--- src/main/java/com/karthick/todomanager/controller/TodosController.java
+++ src/main/java/com/karthick/todomanager/controller/TodosController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-public class TodoController {
+public class TodosController {
     @Autowired
     TodoRepo todoRepo;
 
--- src/main/java/com/karthick/todomanager/datarepo/TodoRepo.java
+++ src/main/java/com/karthick/todomanager/datarepo/TodoRepo.java
@@ -3,5 +3,4 @@ package com.karthick.todomanager.datarepo;
 import com.karthick.todomanager.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TodoRepo extends JpaRepository<Todo, Integer> {
-}
+public interface TodoRepo extends JpaRepository<Todo, Integer> {}
